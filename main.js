7bc4f25ccd82f2922649937f95213249d4c59324
Júnior Cota <jrztec7@gmail.com>
2024-04-05 10:49:41 -0300

    Login funcional com alerta de sucesso

--- main.js
+++ main.js
@@ -7,17 +7,35 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-  // Função para validar o formulário de login antes do envio
-  document.getElementById('loginForm').addEventListener('submit', function(event) {
-    // Obtém os valores dos campos de email e senha
-    var emailValue = document.getElementById('exampleInputEmail1').value;
-    var passwordValue = document.getElementById('exampleInputPassword1').value;
+// Função para realizar o login
+function fazerLogin() {
+  // Pegar os valores dos campos de email e senha
+  var email = document.getElementById('exampleInputEmail1').value;
+  var senha = document.getElementById('exampleInputPassword1').value;
 
-    // Verifica se os campos estão vazios
-    if (emailValue === '' || passwordValue === '') {
-      // Mostra a mensagem de erro
-      document.getElementById('loginErrorMessage').style.display = 'block';
-      // Impede o envio do formulário
-      event.preventDefault();
+  // Verificar se os campos estão preenchidos
+  if (email && senha) {
+    // Verificar se o email e a senha correspondem aos valores armazenados no Local Storage
+    var storedEmail = localStorage.getItem('email');
+    var storedPassword = localStorage.getItem('senha');
+
+    if (email === storedEmail && senha === storedPassword) {
+      alert('Login realizado com sucesso!');
+      // Fechar o modal após o login bem-sucedido
+      $('#loginModal').modal('hide');
+    } else {
+      // Exibir mensagem de erro se as credenciais estiverem incorretas
+      document.getElementById('loginErrorMessage').innerText = 'Credenciais inválidas. Por favor, tente novamente.';
     }
-  });
+  } else {
+    // Exibir mensagem de erro se os campos não estiverem preenchidos
+    document.getElementById('loginErrorMessage').innerText = 'Por favor, preencha todos os campos.';
+  }
+}
+
+// Adicionar evento de submit ao formulário de login
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevenir o envio padrão do formulário
+  fazerLogin(); // Chamar a função de login
+});
+
